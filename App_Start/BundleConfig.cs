1118e5b68217953e2c845984a1c69660605f8266
san7201 <simon_sanchez82192@elpoli.edu.co>
2022-10-27 01:04:53 -0500

    (Style/Feat) Finished layout view
    - Added avatar icons regarding to diferent roles there are (teacher, student, admin)
    - Defined colors to diferent roles:
    Admin: purple
    Teacher: light blue
    Student: gray
    - Added collapse sidebar functionality and transitions to these
    - Stylized sidebar with new colors pallete and change order of menu links
    - Changed old logo to another according to company focus

--- App_Start/BundleConfig.cs
+++ App_Start/BundleConfig.cs
@@ -9,7 +9,7 @@ namespace EduAx
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                        "~/Scripts/jquery-{version}.js", "~/Scripts/Site.js"));
 
             bundles.Add(new StyleBundle("~/styles/layout").Include(
                       "~/Content/Site.css"));
