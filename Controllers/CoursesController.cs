5c8cb933131c25f18be4a04001805133a33f0699
san7201 <simon_sanchez82192@elpoli.edu.co>
2022-12-07 18:18:55 -0500

    (Style) Finished modal "My grades" view

--- Controllers/CoursesController.cs
+++ Controllers/CoursesController.cs
@@ -25,45 +25,59 @@ namespace EduAx.Controllers
             var oUser = (EduAx.Models.PERSON)Session["user"];
             //GENERAL COURSES INFO SHOWED TO ALL USERS
             List<Global> oCourse = (from c in db.COURSE
-                                          join gc in db.GROUP_COURSE on c.ID_COURSE equals gc.ID_COURSE                                      
-                                          where c.ID_COURSE == id_course
-                                          select new Global
-                                          {
-                                              id_course = (int)id_course,
-                                              name_course = c.NAME_COURSE,
-                                              type_course = c.TYPE_COURSE,
-                                              info_course = c.INFO_COURSE,
-                                              icon_course = c.ICON_COURSE,
-                                              image_course = c.IMAGE_COURSE,
-                                              places_groupcourse = (int)gc.PLACES_GROUPCOURSE,
-                                              groups_count = (from gc in db.GROUP_COURSE
-                                                              where gc.ID_COURSE == id_course
-                                                              select gc).Count(),                                             
-                                          }).ToList();
-            for(int i = 1; i < oCourse.Count; i++)
+                                    join gc in db.GROUP_COURSE on c.ID_COURSE equals gc.ID_COURSE
+                                    where c.ID_COURSE == id_course
+                                    select new Global
+                                    {
+                                        id_course = (int)id_course,
+                                        name_course = c.NAME_COURSE,
+                                        type_course = c.TYPE_COURSE,
+                                        info_course = c.INFO_COURSE,
+                                        icon_course = c.ICON_COURSE,
+                                        image_course = c.IMAGE_COURSE,
+                                        places_groupcourse = (int)gc.PLACES_GROUPCOURSE,
+                                        groups_count = (from gc in db.GROUP_COURSE
+                                                        where gc.ID_COURSE == id_course
+                                                        select gc).Count(),
+                                    }).ToList();
+            for (int i = 1; i < oCourse.Count; i++)
             {
                 oCourse.ElementAt(0).places_groupcourse += oCourse.ElementAt(i).places_groupcourse;
                 //oCourse.RemoveAt(i);
-            }                        
+            }
             if (oUser != null)
             {
                 switch (oUser.ROLE_PERSON.ToLower())
                 {
                     case "student":
                         List<Global> oStudent = (from sc in db.STUDENT_COURSE
-                                  where sc.ID_STUDENT == oUser.ID_PERSON && sc.ID_COURSE == id_course
-                                  select new Global
-                                  {
-                                      id_group = (int)sc.ID_GROUP,
-                                      stgrade = (float)sc.STGRADE_STUDENTCOURSE,
-                                      ndgrade = (float)sc.NDGRADE_STUDENTCOURSE,
-                                      rdgrade = (float)sc.RDGRADE_STUDENTCOURSE,
-                                      thgrade = (float)sc.THGRADE_STUDENTCOURSE,
-                                      course_count = (int)sc.TIMES_STUDENTCOURSE,
-                                      state_student = sc.STATE_STUDENTCOURSE
-                                  }).ToList();
+                                                 where sc.ID_STUDENT == oUser.ID_PERSON && sc.ID_COURSE == id_course
+                                                 select new Global
+                                                 {
+                                                     id_group = (int)sc.ID_GROUP,
+                                                     stgrade = (float)sc.STGRADE_STUDENTCOURSE,
+                                                     ndgrade = (float)sc.NDGRADE_STUDENTCOURSE,
+                                                     rdgrade = (float)sc.RDGRADE_STUDENTCOURSE,
+                                                     thgrade = (float)sc.THGRADE_STUDENTCOURSE,
+                                                     course_count = (int)sc.TIMES_STUDENTCOURSE,
+                                                     state_student = sc.STATE_STUDENTCOURSE
+                                                 }).ToList();
                         if (oStudent.Count() > 0)
                         {
+                            if (oStudent.ElementAt(0).state_student.ToLower() != "flunked" &&
+                                oStudent.ElementAt(0).state_student.ToLower() != "cancelled" &&
+                                oStudent.ElementAt(0).state_student.ToLower() != "done")
+                            {
+                                var oId_group = (decimal)oStudent.ElementAt(0).id_group;
+                                List<Global> oTeacher = (from p in db.PERSON
+                                                         join t in db.TEACHER on p.ID_PERSON equals t.ID_TEACHER
+                                                         where oId_group == t.ID_GROUP
+                                                         select new Global
+                                                         {
+                                                             name_person = p.NAME_PERSON
+                                                         }).ToList();
+                                oCourse.ElementAt(0).name_person = oTeacher.ElementAt(0).name_person;
+                            }
                             oCourse.ElementAt(0).id_group = oStudent.ElementAt(0).id_group;
                             oCourse.ElementAt(0).stgrade = oStudent.ElementAt(0).stgrade;
                             oCourse.ElementAt(0).ndgrade = oStudent.ElementAt(0).ndgrade;
@@ -71,8 +85,10 @@ namespace EduAx.Controllers
                             oCourse.ElementAt(0).thgrade = oStudent.ElementAt(0).thgrade;
                             oCourse.ElementAt(0).course_count = (int)oStudent.ElementAt(0).course_count;
                             oCourse.ElementAt(0).state_student = oStudent.ElementAt(0).state_student;
-                        } else
+                        }
+                        else
                         {
+                            oCourse.ElementAt(0).name_person = "UNSIGNED";
                             oCourse.ElementAt(0).course_count = 0;
                             oCourse.ElementAt(0).state_student = "TO DO";
                         }
---
+++ Scripts/Dash_Courses.js
@@ -0,0 +1,44 @@
+﻿
+/* Script scrl animation */
+ScrollReveal({
+    reset: false,
+    distance: '60px',
+    duration: 1500,
+    delay: 600
+});
+//MAIN
+ScrollReveal().reveal('.main-title', { delay: 400, origin: 'right' });
+ScrollReveal().reveal('.img-course', { delay: 600, origin: 'top' });
+ScrollReveal().reveal('.course-status', { delay: 500, origin: 'bottom' });
+ScrollReveal().reveal('.course-attempts', { delay: 600, origin: 'bottom' });
+ScrollReveal().reveal('.teacher-status', { delay: 700, origin: 'bottom' });
+ScrollReveal().reveal('.group-status', { delay: 800, origin: 'bottom' });
+ScrollReveal().reveal('.desc', { delay: 900, origin: 'bottom' });
+ScrollReveal().reveal('.content', { delay: 1000, origin: 'bottom' });
+ScrollReveal().reveal('.dash-top a', { delay: 1100, origin: 'bottom' });
+////MODAL
+//ScrollReveal().reveal('.modal-title', { delay: 500, origin: 'right' });
+//ScrollReveal().reveal('hr', { delay: 600, origin: 'bottom' });
+//ScrollReveal().reveal('.modal-img', { delay: 700, origin: 'left' });
+////MODAL -> GRADES
+//ScrollReveal().reveal('.average-grade', { delay: 700, origin: 'bottom', reset: true });
+//ScrollReveal().reveal('.grade1', { delay: 800, origin: 'bottom', reset: true });
+//ScrollReveal().reveal('.grade2', { delay: 900, origin: 'bottom', reset: true });
+//ScrollReveal().reveal('.grade3', { delay: 1000, origin: 'bottom', reset: true });
+//ScrollReveal().reveal('.grade4', { delay: 1100, origin: 'bottom', reset: true });
+//ScrollReveal().reveal('.modal-close', { delay: 1500, origin: 'left', reset: true });
+
+//OPEN-MODAL
+const openModal = document.querySelector('.hero-cta');
+const modal = document.querySelector('.modal');
+const closeModal = document.querySelector('.modal-close');
+
+openModal.addEventListener('click', (e) => {
+    e.preventDefault();
+    modal.classList.add('modal-show');
+});
+
+closeModal.addEventListener('click', (e) => {
+    e.preventDefault();
+    modal.classList.remove('modal-show');
+});
