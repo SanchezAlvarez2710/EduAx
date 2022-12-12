c2b69dbe03a61f1a0fb9bb41969c9bb163958ef3
san7201 <simon_sanchez82192@elpoli.edu.co>
2022-12-12 17:10:36 -0500

    (Style/Feat) Changes in teacher dashboard view
    - Listed students list regarding teacher group assigned
    - Added new functionality to download students list on PDF format
    - Initialized grade changing from student

--- Controllers/TeacherController.cs
+++ Controllers/TeacherController.cs
@@ -14,79 +14,96 @@ namespace EduAx.Controllers
         eduAxDatabaseEntities db = new eduAxDatabaseEntities();
         // GET: Teacher
         public ActionResult Index()
-        {           
-            var oUser = (EduAx.Models.PERSON)Session["user"];
-            var oGroups = (from p in db.PERSON
-                            join t in db.TEACHER on p.ID_PERSON equals t.ID_TEACHER
-                            where oUser.ID_PERSON == t.ID_TEACHER
-                            select p).ToList();            
-            var oStudents = (from t in db.TEACHER
-                             join sc in db.STUDENT_COURSE on t.ID_GROUP equals sc.ID_GROUP                             
-                             select t).Count();
-            var oCourses = (from gc in db.GROUP_COURSE
-                            join t in db.TEACHER on gc.ID_GROUP equals t.ID_GROUP
-                            where t.ID_TEACHER == oUser.ID_PERSON
-                            select gc).ToList();
-            //ELIMINA LOS CURSOS REPETIDOS
-            for(int i = 0; i < oCourses.Count; i++)
+        {
+            if (Session["user"] != null)
             {
-                for(int j = i + 1; j < oCourses.Count(); j++)
+                var oUser = (EduAx.Models.PERSON)Session["user"];
+                var oGroups = (from p in db.PERSON
+                               join t in db.TEACHER on p.ID_PERSON equals t.ID_TEACHER
+                               where oUser.ID_PERSON == t.ID_TEACHER
+                               select p).ToList();
+                var oStudents = (from t in db.TEACHER
+                                 join sc in db.STUDENT_COURSE on t.ID_GROUP equals sc.ID_GROUP
+                                 select t).Count();
+                var oCourses = (from gc in db.GROUP_COURSE
+                                join t in db.TEACHER on gc.ID_GROUP equals t.ID_GROUP
+                                where t.ID_TEACHER == oUser.ID_PERSON
+                                select gc).ToList();
+                //ELIMINA LOS CURSOS REPETIDOS
+                for (int i = 0; i < oCourses.Count; i++)
                 {
-                    if(oCourses.ElementAt(j).ID_COURSE == oCourses.ElementAt(i).ID_COURSE)
+                    for (int j = i + 1; j < oCourses.Count(); j++)
                     {
-                        oCourses.RemoveAt(j);
+                        if (oCourses.ElementAt(j).ID_COURSE == oCourses.ElementAt(i).ID_COURSE)
+                        {
+                            oCourses.RemoveAt(j);
+                        }
                     }
                 }
-            }
-            //IMPORTA LOS CURSOS RELACIONADOS CON EL PROFESOR
-            var oGlobal = (from gc in db.GROUP_COURSE
-                           join t in db.TEACHER on gc.ID_GROUP equals t.ID_GROUP
-                           where t.ID_TEACHER == oUser.ID_PERSON
-                           select new Global
-                           {
-                               id_course = (int)gc.ID_COURSE,
-                               id_group = (int)gc.ID_GROUP,
-                               places_groupcourse = (int)gc.PLACES_GROUPCOURSE,
-                               state_groupcourse = gc.STATE_GROUPCOURSE
-                           }).ToList();  
-            oGlobal.Add(new Global
+                //IMPORTA LOS CURSOS RELACIONADOS CON EL PROFESOR
+                var oGlobal = (from gc in db.GROUP_COURSE
+                               join t in db.TEACHER on gc.ID_GROUP equals t.ID_GROUP
+                               where t.ID_TEACHER == oUser.ID_PERSON
+                               select new Global
+                               {
+                                   id_course = (int)gc.ID_COURSE,
+                                   id_group = (int)gc.ID_GROUP,
+                                   places_groupcourse = (int)gc.PLACES_GROUPCOURSE,
+                                   state_groupcourse = gc.STATE_GROUPCOURSE
+                               }).ToList();
+                oGlobal.Add(new Global
+                {
+                    name_person = oGroups.ElementAt(0).NAME_PERSON,
+                    groups_count = oGroups.Count,
+                    students_count = oStudents,
+                    courses_count = oCourses.Count()
+                });
+                return View(oGlobal);
+            } else
             {
-                name_person = oGroups.ElementAt(0).NAME_PERSON,
-                groups_count = oGroups.Count,
-                students_count = oStudents,
-                courses_count = oCourses.Count()
-            });
-            return View(oGlobal);
+                return RedirectToAction("Index", "Home");
+            }
         }
 
         public ActionResult Dash(int id_group, string state_group)
         {
-            var oStudents = (from sc in db.STUDENT_COURSE                             
-                             where sc.ID_GROUP == id_group
-                             select new Global
-                             {
-                                 id_group = id_group,
-                                 id_student = (int)sc.ID_STUDENT,
-                                 stgrade = sc.STGRADE_STUDENTCOURSE,
-                                 ndgrade = sc.NDGRADE_STUDENTCOURSE,
-                                 rdgrade = sc.RDGRADE_STUDENTCOURSE,
-                                 thgrade = sc.THGRADE_STUDENTCOURSE,
-                                 times_studentcourse = (int)sc.TIMES_STUDENTCOURSE,
-                                 state_student = sc.STATE_STUDENTCOURSE,
-                                 state_groupcourse = state_group
-                             }).ToList();
-            if (!(oStudents.Count > 0))
+            if (Session["user"] != null)
             {
-                var oGroup = (from gc in db.GROUP_COURSE
-                              where gc.ID_GROUP == id_group
-                              select new Global
-                              {
-                                  id_group = (int)gc.ID_GROUP,
-                                  state_groupcourse = gc.STATE_GROUPCOURSE,
-                              }).ToList();
-                return View(oGroup);
+                var oStudents = (from sc in db.STUDENT_COURSE
+                                 join p in db.PERSON on sc.ID_STUDENT equals p.ID_PERSON
+                                 join c in db.COURSE on sc.ID_COURSE equals c.ID_COURSE
+                                 where sc.ID_GROUP == id_group
+                                 select new Global
+                                 {
+                                     name_course = c.NAME_COURSE,
+                                     id_group = id_group,
+                                     id_student = (int)sc.ID_STUDENT,
+                                     name_person = p.NAME_PERSON,
+                                     stgrade = sc.STGRADE_STUDENTCOURSE,
+                                     ndgrade = sc.NDGRADE_STUDENTCOURSE,
+                                     rdgrade = sc.RDGRADE_STUDENTCOURSE,
+                                     thgrade = sc.THGRADE_STUDENTCOURSE,
+                                     times_studentcourse = (int)sc.TIMES_STUDENTCOURSE,
+                                     state_student = sc.STATE_STUDENTCOURSE,
+                                     state_groupcourse = state_group
+                                 }).ToList();
+                if (!(oStudents.Count > 0))
+                {
+                    var oGroup = (from gc in db.GROUP_COURSE
+                                  where gc.ID_GROUP == id_group
+                                  select new Global
+                                  {
+                                      id_group = (int)gc.ID_GROUP,
+                                      state_groupcourse = gc.STATE_GROUPCOURSE,
+                                  }).ToList();
+                    return View(oGroup);
+                }
+                return View(oStudents);
             }
-            return View(oStudents);
+            else
+            {
+                return RedirectToAction("Index", "Home");
+            }
         }
     }
 }
--- Controllers/TrayectoryController.cs
+++ Controllers/TrayectoryController.cs
@@ -15,29 +15,31 @@ namespace EduAx.Controllers
         public ActionResult Index()
         {
             var oUser = (EduAx.Models.PERSON)Session["user"];
-            //List<List<EduAx.Models.ViewModel.Global>> oGlobal = new List<List<EduAx.Models.ViewModel.Global>>();   
-            List<EduAx.Models.ViewModel.Global> oStudent = (from sc in db.STUDENT_COURSE
-                                                            join c in db.COURSE on sc.ID_COURSE equals c.ID_COURSE
-                                                          where oUser.ID_PERSON == sc.ID_STUDENT
-                                                          select new Global
-                                                          {
-                                                              id_student = (int)sc.ID_STUDENT,
-                                                              id_course = (int)sc.ID_COURSE,
-                                                              icon_course = c.ICON_COURSE,
-                                                              name_course = c.NAME_COURSE,
-                                                              id_group = (int)sc.ID_GROUP,
-                                                              stgrade = (int)sc.STGRADE_STUDENTCOURSE,
-                                                              ndgrade = (int)sc.NDGRADE_STUDENTCOURSE,
-                                                              rdgrade = (int)sc.RDGRADE_STUDENTCOURSE,
-                                                              thgrade = (int)sc.THGRADE_STUDENTCOURSE,
-                                                              times_studentcourse = (int)sc.TIMES_STUDENTCOURSE,
-                                                              state_student = sc.STATE_STUDENTCOURSE
-                                                          }).ToList();
-            string[] oCourses = {"FIRST AID BASICS", "RCP TECHNIQUES", "CLINIC REPORTS", "PHARMACOLOGY"};            
-            
-            //oGlobal.Add(oStudent);
-
-            return View(oStudent);
+            if (oUser != null)
+            {                  
+                List<EduAx.Models.ViewModel.Global> oStudent = (from sc in db.STUDENT_COURSE
+                                                                join c in db.COURSE on sc.ID_COURSE equals c.ID_COURSE
+                                                                where oUser.ID_PERSON == sc.ID_STUDENT
+                                                                select new Global
+                                                                {
+                                                                    id_student = (int)sc.ID_STUDENT,
+                                                                    id_course = (int)sc.ID_COURSE,
+                                                                    icon_course = c.ICON_COURSE,
+                                                                    name_course = c.NAME_COURSE,
+                                                                    id_group = (int)sc.ID_GROUP,
+                                                                    stgrade = (int)sc.STGRADE_STUDENTCOURSE,
+                                                                    ndgrade = (int)sc.NDGRADE_STUDENTCOURSE,
+                                                                    rdgrade = (int)sc.RDGRADE_STUDENTCOURSE,
+                                                                    thgrade = (int)sc.THGRADE_STUDENTCOURSE,
+                                                                    times_studentcourse = (int)sc.TIMES_STUDENTCOURSE,
+                                                                    state_student = sc.STATE_STUDENTCOURSE
+                                                                }).ToList();
+                return View(oStudent);
+            }
+            else
+            {
+                return RedirectToAction("Index", "Home");
+            }
         }
     }
 }
