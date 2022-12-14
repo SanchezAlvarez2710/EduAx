bd0ae96c343229e57ab6ec28f56619c0369cc09c
san7201 <simon_sanchez82192@elpoli.edu.co>
2022-12-13 23:54:02 -0500

    (Perf) Setted views links regarding user role

--- Controllers/AdminController.cs
+++ Controllers/AdminController.cs
@@ -1,16 +1,10 @@
-<<<<<<< HEAD
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-=======
-﻿using EduAx.Models;
+using EduAx.Models;
 using EduAx.Models.ViewModel;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Security.Cryptography;
 using System.Security.Permissions;
->>>>>>> 61c7f9e ((Feat) Added feature "Dashboard" as Admin)
 using System.Web;
 using System.Web.Mvc;
 
@@ -18,15 +12,6 @@ namespace EduAx.Controllers
 {
     public class AdminController : Controller
     {
-<<<<<<< HEAD
-        // GET: Admin
-        public ActionResult Index()
-        {
-            return View();
-        }
-    }
-}
-=======
         eduAxDatabaseEntities db = new eduAxDatabaseEntities();
         // GET: Admin
         public ActionResult Index()
@@ -34,7 +19,7 @@ namespace EduAx.Controllers
             if (Session["user"] != null)
             {
                 var oUser = (EduAx.Models.PERSON)Session["user"];
-                
+
                 var oStudents = (from t in db.PERSON
                                  where t.ROLE_PERSON.Equals("STUDENT")
                                  select t).Count();
@@ -60,4 +45,4 @@ namespace EduAx.Controllers
         }
     }
 }
->>>>>>> 61c7f9e ((Feat) Added feature "Dashboard" as Admin)
+
